248c585182a7d13e5956fd76214d770bc128d346
Kyle Swanson <swansonk@mac.com>
2023-04-08 12:42:32 -0500

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Kyle Swanson on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
